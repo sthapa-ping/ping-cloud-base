4c54a947dbd189273edd32319feceaeef607a2bf
savithaganapathi <savithaganapathi@pingidentity.com>
2020-11-19 15:14:20 -0600

    PDO-1564: Replacing FluxCD with ArgoCD as the CD tool in Ping Cloud
    Adding git-remote-codecommit to the ArgoCD repo server using the same
    image as the app container
    
    [skip pipeline]

--- k8s-configs/cluster-tools/git-ops/argo/install-git-remote-codecommit.sh
+++ k8s-configs/cluster-tools/git-ops/argo/install-git-remote-codecommit.sh
@@ -3,9 +3,7 @@
 pip install --upgrade pip
 pip3 install git-remote-codecommit --no-warn-script-location
 
-cp /usr/local/bin/git-remote-codecommit /tools
 cp /usr/local/lib/python3.7/site-packages/git_remote_codecommit/__init__.py /tools
 
-# On the ArgoCD container, python3 is available under /usr/bin/python3
-sed -i 's|/usr/local/bin/python|/usr/bin/python3|' /tools/git-remote-codecommit
+cp /usr/local/bin/git-remote-codecommit /tools
 chmod a+x /tools/git-remote-codecommit
