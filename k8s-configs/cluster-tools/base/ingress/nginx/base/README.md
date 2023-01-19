28aa8e230ceec505a46df8626a16e32cfaf89b07
suraj thapa <surajthapa@pingidentity.com>
2023-01-19 08:30:09 -0700

    Updates

--- k8s-configs/cluster-tools/base/ingress/nginx/base/README.md
+++ k8s-configs/cluster-tools/base/ingress/nginx/base/README.md
@@ -1,6 +1,7 @@
 # Ingress Controller Base Yaml
 The base yaml is obtained from:
-https://github.com/kubernetes/ingress-nginx/blob/main/deploy/static/provider/aws/deploy.yaml
+https://github.com/kubernetes/ingress-nginx/blob/main/deploy/static/provider/baremetal/deploy.yaml
 
 
 Run `./update-ingress.sh NEW_VERSION` to upgrade install.yaml
+Example: Example: ./update-ingress.sh v1.5.2 
--- k8s-configs/cluster-tools/base/ingress/nginx/base/update-ingress.sh
+++ k8s-configs/cluster-tools/base/ingress/nginx/base/update-ingress.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-USAGE="./update-ingress.sh INGRESS_CONTROLLER_VERSION"
+USAGE="./update-ingress.sh INGRESS_CONTROLLER_VERSION
+       Example: ./update-ingress.sh v1.5.2"
 REQ_PATH="k8s-configs/cluster-tools/base/ingress/nginx/base"
 
 if [[ ! "$(pwd)" = *"${REQ_PATH}"* ]]; then
@@ -16,6 +17,6 @@ fi
 
 INGRESS_CONTROLLER_VERSION="${1}"
 
-curl "https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-${INGRESS_CONTROLLER_VERSION}/deploy/static/provider/aws/deploy.yaml" -o install.yaml
+curl "https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-${INGRESS_CONTROLLER_VERSION}/deploy/static/provider/baremetal/deploy.yaml" -o install.yaml
 
 echo "Ingress Controller update complete, check your 'git diff' to see what changed"
